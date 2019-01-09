0b22470a7dffff0cc40c338e7a25e41b694c4081
lavandaengineer <45874375+lavandaengineer@users.noreply.github.com>
2019-01-09 17:21:57 +0000

    Update index.js

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,9 +1,11 @@
+const moduleBase = [
+  'eslint-config-airbnb-base',
+  'eslint-config-airbnb-base/rules/strict',
+  './rules/react',
+  './rules/react-a11y',
+];
+
 module.exports = {
-  extends: [
-    'eslint-config-airbnb-base',
-    'eslint-config-airbnb-base/rules/strict',
-    './rules/react',
-    './rules/react-a11y',
-  ].map(require.resolve),
+  extends: moduleBase.map(require.resolve),
   rules: {}
 };
